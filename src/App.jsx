3726f96c2cfb7decbfe33b2b03d5a3b8c988909a
Andres Aristimuño <andaristis@gmail.com>
2023-11-01 20:42:23 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './Componentes/Card'
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card />
+    </div>
+  )
+}
+
+export default App

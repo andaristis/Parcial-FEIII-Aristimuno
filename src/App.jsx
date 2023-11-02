66f3d3682cb466a7c496f3b4a028f1d2456e64f2
Andres Aristimuño <andaristis@gmail.com>
2023-11-02 20:11:27 -0300

    entrega final

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,53 @@
+import { useState } from 'react'
 import './App.css'
 import Card from './Componentes/Card'
 import React from 'react'
 
 function App() {
+  const [nombre, setNombre] = useState('')
+  const [banda, setBanda] = useState('')
+  const [show, setShow] = useState(false)
+  const [error, setError] = useState(false)
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(nombre.length > 3 && banda.length > 6){
+      setShow(true)
+      setError(false)
+  } else {
+      setError(true)
+      setShow(false)
+  }
+}
+
+
   return (
-    <div className="App">
-      <h1>Carga de estudiantes</h1>
-      <form></form>
-      <Card />
-    </div>
+    <>
+      <h1>Encuesta sobre gustos musicales</h1>
+      <div>
+          <form onSubmit={handleSubmit}>
+              <label> Nombre: </label>
+              <input type="text" placeholder='Ingrese nombre'
+                onChange={(e) => {
+                  setNombre(e.target.value.trim());
+                  setShow(false);
+                  setError(false)
+                  }}/>
+              <label> Banda Favorita: </label>
+              <input type="text" placeholder='Ingrese Banda favorita' 
+              onChange={(e) => {
+                setBanda(e.target.value.trim())
+                setShow(false);
+                setError(false)
+                }}/>
+              <button>Listo!</button>
+          </form>
+      </div>
+      <div>
+        {show && <Card nombre={nombre} banda={banda}/>}
+        {error && <h2 className="error">Por favor chequea que la información sea correcta</h2>}
+      </div>
+    </>
   )
 }
 
--- src/Componentes/Card.jsx
+++ src/Componentes/Card.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const Card = () => {
-  return (
-    <div>
-        Hola, soy una tarjeta
+
+
+const Card = ({nombre, banda}) => {
+  const sitioWeb = banda.replace(/\s/g, '');
+  
+ 
+    return (
+    <div className="card">
+        <h2>Hola {nombre}!<br/>Que gran elección, {banda} es una gran banda!<br/><br/>Visítalos en www.{sitioWeb.toLowerCase()}.com</h2>
     </div>
   )
 }
